abd82708ef1ada74d6f08952f012c160cb82d2a9
youbot <youbot@ubuntu.(none)>
2013-06-30 05:21:31 +0200

    Añadida libreria C++ para comunicación serie y adaptación de paquete ROS

--- src/pan_tilt_ptu_46.cpp
+++ src/pan_tilt_ptu_46.cpp
@@ -2,12 +2,7 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
 #include "sensor_msgs/Joy.h"
-extern "C" {
-	#include "serial.h"
-	#include "time.h"
-	#include "error.h"
-}
-
+#include "Serial_Q.h"
 
 #define PI 				3.1415927
 
@@ -41,11 +36,11 @@ void processPtuComm();
 
 ros::NodeHandle* myNodeHandle = 0;
 
+Serial::Serial_Q *Ptu;
+
 char tmpChar;
 char status;
-char* mySerialDevice = "/dev/ttyUSB0";
-int fd;
-queue RX_Q = {0, 0};
+char mySerialDevice[] = "/dev/ttyUSB0";
 
 sensor_msgs::JointState thePtuJointState;
 
@@ -59,19 +54,14 @@ enum estado {
 estado STATUS = IDLE;
 
 
-int envia(int inFd, const char* inStr, int numChars) {
+void envia(int inFd, const char* inStr) {
 
-	int i;
-	//printf("Enviando: '%s'\n","I ");
-	write(inFd,"I ",2);
+	Ptu->send("I ");
 	processPtuComm();
-	//printf("Enviando: '%s'\n",inStr);
-	write(inFd,inStr,numChars);
+	Ptu->send("inStr ");
 	processPtuComm();
-	//printf("Enviando: '%s'\n","A ");
-	write(inFd,"A ",2);
+	Ptu->send("A ");
 	processPtuComm();
-
 
 }
 
@@ -94,30 +84,31 @@ int main(int argc, char** argv) {
 
 	// Init PTU-46
 
-	fd = initSerial(SERIALDEVICE,9600,'N',8,1);
-	if (fd < 0) {
-		printf("No se puede abrir dispositivo serie '%s'. Error: '%s'\n",SERIALDEVICE,strerror(errno)) ;
-		return fd;
+	Ptu = new Serial::Serial_Q(mySerialDevice, B9600);
+
+	if (Ptu->LastError != NULL) {
+		ROS_ERROR("Error conectando con unidad PTU-46: %s",Ptu->LastError);
+		return -1;
 	}
 
-	ROS_INFO("Puerto serie abierto");
+	ROS_INFO("Puerto serie abierto %d",B9600);
 
 	// Enviamos comando de modo de ejecución inmediato
 
-	// envia(fd,"I ",2);	// Modo inmediato
-	// envia(fd,"FT ",3);	// Respuestas escuetas
+	Ptu->send("I ");	// Modo inmediato
+	Ptu->send("FT ");	// Respuestas escuetas
 
 	ros::Rate r(1); // 1 hz
 	while (myNodeHandle->ok()) {
 
 /*
 		// Enviamos al PTU petición de posición PAN
-		envia(fd,"PP\n",3);
+		Ptu->send("PP\n");
 		STATUS = WAIT_POS_PAN;
 		processPtuComm();
 
 		// Enviamos al PTU petición de posición TILT
-		envia(fd,"TP\n",3);
+		Ptu->send("TP\n");
 		STATUS = WAIT_POS_TILT;
 		processPtuComm();
 */
@@ -135,8 +126,8 @@ int main(int argc, char** argv) {
 bool getPosCommand(float inDeg, int inTargetJoint, int inMode, char* outCommand) {
 
 	int thePosVal;
-	char theParam;
-	char theMode;
+	char theParam = 'P';
+	char theMode = ABSOLUTE;
 	bool ok = true;
 
 	switch(inTargetJoint) {
@@ -184,11 +175,11 @@ void posCallback(const sensor_msgs::JointState& inJointState) {
 	theBuildCommandOk = theBuildCommandOk && getPosCommand(theTiltDeg,TILT,ABSOLUTE,theTiltCommand);
 
 	if (theBuildCommandOk) {
-		if (envia(fd,thePanCommand,strlen(thePanCommand)) > 0) {
+		if (Ptu->send(thePanCommand) > 0) {
 			STATUS = WAIT_COMMAND_CONF;
 			processPtuComm();
 		}
-		if (envia(fd,theTiltCommand,strlen(theTiltCommand)) > 0) {
+		if (Ptu->send(theTiltCommand) > 0) {
 			STATUS = WAIT_COMMAND_CONF;
 			processPtuComm();
 		}
@@ -214,7 +205,7 @@ void joyCallback(const sensor_msgs::Joy& inJoyCommand) {
 
 	if (theAxesH != 0.0) {
 		getPosCommand(theAxesH*JOYSTEP_PAN_DEG,PAN,RELATIVE,thePanCommand);
-		if (envia(fd,thePanCommand,strlen(thePanCommand)) > 0) {
+		if (Ptu->send(thePanCommand) > 0) {
 			STATUS = WAIT_COMMAND_CONF;
 			processPtuComm();
 		}
@@ -222,7 +213,7 @@ void joyCallback(const sensor_msgs::Joy& inJoyCommand) {
 
 	if (theAxesV != 0.0) {
 		getPosCommand(theAxesV*JOYSTEP_TILT_DEG,TILT,RELATIVE,theTiltCommand);
-		if (envia(fd,theTiltCommand,strlen(theTiltCommand)) > 0) {
+		if (Ptu->send(theTiltCommand) > 0) {
 			STATUS = WAIT_COMMAND_CONF;
 			processPtuComm();
 		}
@@ -243,48 +234,32 @@ void mydelay_ms(int milliseconds) {
 
 }
 
+bool extractInt(const string inStr, int* outNum) {
+
+	stringstream ss(inStr);
+	string tmp;
+	ss >> tmp >> *outNum;
+	return true;
+
+}
 
 void processPtuComm() {
 
-	int bytesReceived;
-	int i;
-	char c;
-	float deg;
+	string theResp;
+	std::size_t theFound;
 
-	/*
-	 * Comprobar datos serie. Esta conexión serie es no bloqueante
-	 * De modo que retornará inmediatamente
-	 * devolviendo un código de error concreto cuando no haya datos disponibles para lectura
-	 * Si hay datos disponibles se introducen en una cola para su posterior procesamiento.
-	 */
+	if (Ptu->checkDataAndEnqueue()) {
 
+		theResp.assign((char *)Ptu->getFullQueueContent(true));
 
-	char buf[255];
-	bytesReceived = read(fd,buf,255);
-	if (bytesReceived < 0) {
-		//printf("[Fer debug] Ignorando: %s\n", strerror(errno));
-		errno = 0;
-	}
-	while(bytesReceived > 0) {
-		printf("[Fer debug] Recibidos: %d bytes\n", bytesReceived);
-		for (i=0;i<bytesReceived;i++) {
-			RX_enqueue(&RX_Q,buf[i]);
+		// Un signo de exclamación indica que se ha producido un error
+
+		theFound = theResp.find("!");
+		if (theFound != string::npos) {
+			STATUS = IDLE;
+			ROS_ERROR("PTU-46 ha devuelto un error: %s",theResp.c_str());
+			return;
 		}
-		bytesReceived = read(fd,buf,255);
-		if (bytesReceived < 0) {
-			//printf("[Fer debug] Ignorando: %s\n", strerror(errno));
-			errno = 0;
-		}
-	}
-
-
-	/*
-	 * Comprobar los datos recibidos mediante puerto serie
-	 */
-
-	printf("[Fer debug] Estado %d. Elementos en cola: %d: %s\n", STATUS, RX_Q.numElem, RX_showContents(&RX_Q));
-
-	if(RX_Q.numElem > 0) {
 
 		switch (STATUS) {
 
@@ -293,91 +268,51 @@ void processPtuComm() {
 				// Recepción inesperada
 				// Mostramos datos recibidos
 
-				printf("Datos inesperados: %s",RX_toStr(&RX_Q));
+				ROS_ERROR("PTU-46 ha enviado un dato inesperado: %s",theResp.c_str());
 
 				break;
 
 			case WAIT_COMMAND_CONF:
 
-				do {
-					c = RX_dequeue(&RX_Q);
-				} while ((c != '!') && (c != '*') && (RX_Q.numElem > 0));
-
-				switch(c) {
-					case '!':
-						// Error en el comando
-						if(RX_Q.numElem > 0) printf("Error en el último comando: %s",RX_toStr(&RX_Q));
-						break;
-					case '*':
-						// Comando confirmado
-						printf("Ok\n");
-						break;
-					default:
-						// Inesperado
-						break;
+				theFound = theResp.find("*");
+				if (theFound != string::npos) {
+					ROS_INFO("PTU-46 ha confirmado la última orden: %s",theResp.c_str());
+					STATUS = IDLE;
 				}
 
-				printf("%s\n",RX_toStr(&RX_Q));
-
-				STATUS = IDLE;
 				break;
 
 			case WAIT_POS_PAN:
+
+				break;
+
 			case WAIT_POS_TILT:
 
-				// Extraer la información del mensaje de respuesta
-				// Ejemplo de mensaje esperado:
-				// * Current Pan position is -2500
+				// Comprobamos que el comando se ha admitido
+				// Y eliminamos el asterisco de confirmación
 
-				int pos;
+				int thePos;
 
-				do {
-					c = RX_dequeue(&RX_Q);
-				} while ((c != '!') && (c != '*') && (RX_Q.numElem > 0));
+				theFound = theResp.find("*");
+				if (theFound != string::npos) {
+					theResp.erase(0,theFound+1);
+					STATUS = IDLE;
+				}
 
-				switch(c) {
-					case '!':
-						// Error en el comando
-						if(RX_Q.numElem > 0) printf("Error en el último comando: %s",RX_toStr(&RX_Q));
-						break;
-					case '*':
-						// Comando confirmado
-						if (RX_Q.numElem == 0) {
-							STATUS = IDLE;
-							return;
-						}
-						//  Avanzamos en la cola hasta encontrar dígitos o signo '-'
-						do {
-							c = RX_dequeue(&RX_Q);
-						} while ((RX_Q.numElem > 0)&&(!(c == '-')||((c >= '0')&&(c <= '9'))));
-						if (RX_Q.numElem == 0) {
-							STATUS = IDLE;
-							return;
-						}
-						char posNumber[16];
-						i = 1;
-						posNumber[0] = c;
-						while ((RX_Q.numElem > 0)&&(i<16)) {
-							c = RX_dequeue(&RX_Q);
-							if ((c >= '0')&&(c <= '9')) {
-								posNumber[i++] = c;
-							} else {
-								break;
-							}
-						}
-						posNumber[i] = '\0';
-						pos = atoi(posNumber);
-						if (STATUS == WAIT_POS_PAN) {
-							deg = (float)pos * PAN_RESOLUTION / 3600;
-							thePtuJointState.position[0] = deg;
-						} else if (STATUS == WAIT_POS_TILT) {
-							deg = (float)pos * TILT_RESOLUTION / 3600;
-							thePtuJointState.position[1] = deg;
-						}
-						break;
-					default:
-						// Inesperado
-						break;
+				// Leemos el dato devuelto
+
+				if (extractInt(theResp,&thePos)) {
+
+					float theDeg;
+
+					if (STATUS == WAIT_POS_PAN) {
+						theDeg = (float)thePos * PAN_RESOLUTION / 3600;
+						thePtuJointState.position[0] = theDeg;
+					} else if (STATUS == WAIT_POS_TILT) {
+						theDeg = (float)thePos * TILT_RESOLUTION / 3600;
+						thePtuJointState.position[1] = theDeg;
+					}
+
 				}
 
 				STATUS = IDLE;
